b7c2885cb81af8866e1cb0d318f54edb7afd7ec0
cicenet <cicenet@gmail.com>
2017-11-14 21:42:48 -0500

    Appending some comments according to feedback of peer review

--- cachematrix.R
+++ cachematrix.R
@@ -4,13 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  # Assign NULL to a global variable "m" in which is stored inverse matrix
   m <- NULL
   set <- function(y) {
+    #Assign values to global variables
    x <<- y
    m <<- NULL
   }
  get <- function() x
+  #Inverse matrix is assigned to global variable
  setsolve <- function(solve) m <<- solve
+  # Return the inverse matrix
  getsolve <- function() m
  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
@@ -19,10 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 cachesolve <- function(x, ...) {
 ## Return a matrix that is the inverse of 'x'
     m <- x$getsolve()
+  #Validation if the inverse matrix was already calculated
   if(!is.null(m)) {
      message("getting cached inverse matrix")
-    return(m)
+    #Return the inverse matrix and exit the function
+     return(m)
     }
+  #If inverse matrix was not calculated, then is calculated for once
     data <- x$get()
     m <- solve(data, ...)
     x$setsolve(m)
